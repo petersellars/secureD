7ba58a74192cc615d00aa9f98a9c478d3a35e8fe
Pete Sellars <psellars@gmail.com>
2015-09-19 12:56:21 +1200

    Run docker-benchmark from Go using dockerclient

--- README.md
+++ README.md
@@ -4,8 +4,17 @@ Docker Hack Day #3 Project to provide simple use of trusted images
 
 Take the stress out of ensuring trusted images are used by providing a secureD wrapper around Docker. Set up of trust and storage of keys in a vault should be simple and ensure pull, push and build interactions with registries uses trusted capabilities
 
+### Currently Uses
+* [docker-bench-security](https://hub.docker.com/r/docker/docker-bench-security/) from the [Docker Hub](https://hub.docker.com)
+* [dockerclient](https://github.com/samalba/dockerclient) for Go
+
 ### ToDo
-* Run [dockerbench](http://github.com/docker/docker-bench-security) from secureD
+* Use a local [Docker Registry](https://hub.docker.com/_/registry/) for testing content trust locally
+* Add a benchmark command
+* Parse [dockerbench](http://github.com/docker/docker-bench-security) file and return results
+
+### Does
+* Runs [dockerbench](http://github.com/docker/docker-bench-security) from secureD
 
 ### Ideas:
 * Wrapper around [Docker Engine](https://github.com/docker/docker) using content trust
--- main.go
+++ main.go
@@ -1,28 +1,64 @@
 package main
 
 import (
-	"os"
-	"log"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/samalba/dockerclient"
 )
 
 func main() {
-	
-	// Initiate the Docker Client
+
+	// initiate the docker client
 	docker, _ := dockerclient.NewDockerClient("unix:///var/run/docker.sock", nil)
+	// set the docker content trust environment variable
+	os.Setenv("docker_content_trust", "1")
+	fmt.Println("docker_content_trust:", os.Getenv("docker_content_trust"))
 
-	// Set the Docker Content Trust environment variable
-	os.Setenv("DOCKER_CONTENT_TRUST","1")
-	fmt.Println("DOCKER_CONTENT_TRUST:",os.Getenv("DOCKER_CONTENT_TRUST"))
-
-	// Useful Information during hack
-    versionInfo, _ := docker.Version()
-	log.Println(versionInfo)
-	dockerVersion := versionInfo.Version
-	log.Println(dockerVersion)
+	// useful information during hack
+	versioninfo, _ := docker.Version()
+	log.Println(versioninfo)
+	dockerversion := versioninfo.Version
+	log.Println(dockerversion)
 
 	info, _ := docker.Info()
 	log.Println(info)
+
+	// secureD
+
+	// Create the benchmark container
+	// TODO - currently depends on benchmark being available
+	
+    hostConfig := &dockerclient.HostConfig {
+		Binds: []string{"/var/lib:/var/lib",
+			"/var/run/docker.sock:/var/run/docker.sock",
+			"/usr/lib/systemd:/usr/lib/systemd",
+			"/etc:/etc"},
+		NetworkMode: "host",
+		PidMode: "host",
+		CapAdd: []string{"audit_control"},
+	}
+	
+	benchmarkContainerConfig := &dockerclient.ContainerConfig{
+		Image: "docker/docker-bench-security:latest",
+		AttachStdin: true,
+		AttachStdout: true,
+		AttachStderr: true,
+		OpenStdin: true,
+		StdinOnce: true,
+		Tty: true,
+		Labels: map[string]string{"docker_bench_security":""},
+	}
+	containerId, err := docker.CreateContainer(benchmarkContainerConfig, "docker-benchmark")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(containerId)
+
+	// Start the benchmark container
+	err = docker.StartContainer(containerId, hostConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
